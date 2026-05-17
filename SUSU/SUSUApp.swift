c14ef2df008e27d022cc422eea8712d5def18b35
SleepMaker <little230@gmail.com>
2026-05-17 17:21:53 -0400

    Initial Commit

---
+++ SUSU/SUSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUSUApp.swift
+//  SUSU
+//
+//  Created by Dante Little on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SUSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
